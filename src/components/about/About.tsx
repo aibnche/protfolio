7836150a8926d62de1e25ad2edb76b2b4e36e787
aibn-che <bnchraf4@gmail.com>
2025-09-16 20:27:52 +0100

    .

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -2,12 +2,13 @@ import React from 'react'
 import './about.scss'
 import Flicker from "@/components/flicker/Flicker"
 import SlidingCircle from '../slidingCircle/SlidingCircle'
-
+import FireMouse from '../firemouse/FireMouse'
 const About = () => {
 	return (
 	
 	<Flicker>
 		<div className="container about-c">
+			<FireMouse />
 			<div className="intro">
 				<h1>Hi there, I'm Ashraf</h1>
 				
@@ -17,7 +18,7 @@ const About = () => {
 				
 				<p>Currently honing my skills at <strong>42 School</strong> while building projects with the <strong>MERN stack</strong> (MongoDB, Express, React, Node.js) and <strong>Next.js</strong>. I bring designs to life with <strong>CSS/Sass</strong> superpowers and a focus on seamless user experiences.</p>
 				
-				<p>My tech journey started at <strong>OFPPT</strong>, with a passion for problem-solving through code. Now I'm leveling up at <strong>42 School</strong>'s peer-driven learning environment – surviving the infamous "piscine" and embracing the grind <span className="emoji">💻</span></p>
+				<p>My tech journey started at <strong>OFPPT</strong>, with a passion for problem-solving through code. Now I'm leveling up at <strong>42 School</strong>'s peer-driven learning environment – surviving the infamous "piscine" and embracing the grind</p>
 				
 				<p>I thrive where logic meets creativity. Whether it's architecting a full-stack app or tweaking animations until they <em>feel</em> just right, I love the process as much as the result.</p>
 				
